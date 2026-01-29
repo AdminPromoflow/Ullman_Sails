9b88bc23777fff7713091f946fcdb155057599f5
Aleina Rossui <admin@promoflow.net>
2026-01-29 17:33:53 -0500

    mensaje

--- Services-2.SailsCleaning/2.Services/Services.js
+++
@@ -1,8 +0,0 @@
-// Handles optional .OpenContactUs buttons elsewhere
-const buttonTitle = document.querySelectorAll(".OpenContactUs");
-
-for (let i = 0; i < buttonTitle.length; i++) {
-  buttonTitle[i].addEventListener("click", function () {
-    window.open("../ContactUs/index.php", "_self");
-  });
-}
