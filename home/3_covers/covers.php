12f06c447cc3639b4a246045286bf7348e49ff00
Aleina Rossui <admin@promoflow.net>
2026-03-24 12:49:00 -0400

    mensaje

--- home/3_covers/covers.php
+++ home/3_covers/covers.php
@@ -1,11 +1,28 @@
 <?php $cssVer = file_exists(__DIR__ . '/../home/3_covers/covers.css') ? filemtime(__DIR__ . '/../home/3_covers/covers.css') : ''; ?>
+<?php $jsVer = file_exists(__DIR__ . '/../home/3_covers/covers.js') ? filemtime(__DIR__ . '/../home/3_covers/covers.js') : ''; ?>
+
 <link rel="stylesheet" href="../home/3_covers/covers.css<?= $cssVer ? '?v=' . $cssVer : '' ?>">
 
 <section class="covers">
-    <div class="section_covers">
-      <h1>Covers</h1>
+  <div class="section_covers">
+    <div class="img-title-sailing-content">
+      <img
+        src="../cruising_navigator/1_introduction/img/ullman_sails.png"
+        alt="Ullman Sails"
+        decoding="async"
+        width="240"
+        height="72"
+      >
+    </div>
+
+    <h1>Covers</h1>
+
+    <div class="covers_button">
       <a href="../Covers/index.php" class="covers_link">
-      <span class="covers_button">See more</span>
+        <h3>See more</h3>
       </a>
     </div>
+  </div>
 </section>
+
+<script defer src="../home/3_covers/covers.js<?= $jsVer ? '?v=' . $jsVer : '' ?>"></script>
