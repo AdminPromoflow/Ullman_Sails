3fb8a6941ba662a1c522c2bc924210c90e7b2c96
Laura  Rozo <admin@promoflow.net>
2026-04-01 03:22:11 -0500

    “…”

--- New_Repair_Quote/index.php
+++ New_Repair_Quote/index.php
@@ -10,10 +10,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
   </head>
   <body class="aboutUs">
-    <?php include "../General/Menu/Menu.php" ?>
+    <?php include "../general/menu/menu.php" ?>
     <?php include "../New_Repair_Quote/seccion/Repair_Quote.php" ?>
-    <?php include "../General/Charging/charging.php"; ?>
-    <?php include "../General/Footer/Footer.php" ?>
+    <?php include "../general/charging/charging.php"; ?>
+    <?php include "../general/footer/Footer.php" ?>
   </body>
 
 </html>
