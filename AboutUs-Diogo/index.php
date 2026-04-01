f7f4b51190008564fa7d16c3187ed5bd730ef52c
Laura  Rozo <admin@promoflow.net>
2026-04-01 01:35:47 -0500

    “…”

--- AboutUs-Diogo/index.php
+++ AboutUs-Diogo/index.php
@@ -78,7 +78,7 @@
     <?php include "../AboutUs-Diogo/navigation/navigation.php" ?>
     <?php include "2.Services/Services.php" ?>
     </div>
-    <?php include "../general/charging/charging.js"; ?>
+    <?php include "../general/charging/charging.php"; ?>
 
     <div id="searchContent" class="searchContent">
     </div>
