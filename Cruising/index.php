04272cb884f8123cce8413e76bf1080c5ed97ab5
Laura  Rozo <admin@promoflow.net>
2026-04-01 02:34:49 -0500

    “…”

--- Cruising/index.php
+++ Cruising/index.php
@@ -35,13 +35,13 @@
       }
 
     </style>
-    <?php include "../General/Menu/Menu.php" ?>
+    <?php include "../general/menu/menu.php" ?>
 
     <div id="searchHide" class="searchHide">
       <?php // include "navigation/navigation.php"; ?>
-      <?php include "../General/new_sail_quote/new_sail_quote.php"; ?>
+      <?php include "../general/new_sail_quote/new_sail_quote.php"; ?>
 
-      <?php include "../General/arrows_up_down/arrows_up_down.php" ?>
+      <?php include "../general/arrows_up_down/arrows_up_down.php" ?>
       <?php include "navigation/navigation.php" ?>
 
 
@@ -53,13 +53,13 @@
 
     <?php //include "2.Services/Services.php" ?>
     </div>
-    <?php include "../General/Charging/charging.php"; ?>
+    <?php include "../general/charging/charging.php"; ?>
 
 
 
     <div id="searchContent" class="searchContent">
     </div>
-    <?php include "../General/Footer/Footer.php" ?>
+    <?php include "../general/footer/Footer.php" ?>
     <script type="text/javascript">
     // Función para manejar el scroll suave y respuesta rápida
         let isScrolling = false;
