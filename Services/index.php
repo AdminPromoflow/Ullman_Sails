797fe9e5c0036f0a40ec9a8993395a828ab24d97
Laura  Rozo <admin@promoflow.net>
2026-04-01 04:04:30 -0500

    “…”

--- Services/index.php
+++ Services/index.php
@@ -73,19 +73,19 @@
 
     </style>
 
-    <?php include "../General/Menu/Menu.php" ?>
+    <?php include "../general/menu/menu.php" ?>
 
     <div id="searchHide" class="searchHide">
     <?php include "../Services/1.slider/slider.php" ?>
     <?php include "../Services/navigation/navigation.php" ?>
     <?php include "2.Services/Services.php" ?>
     </div>
-    <?php include "../General/Charging/charging.php"; ?>
+    <?php include "../general/charging/charging.php"; ?>
 
 
     <div id="searchContent" class="searchContent">
     </div>
-    <?php include "../General/Footer/Footer.php" ?>
+    <?php include "../general/footer/Footer.php" ?>
     <script type="text/javascript">
     // Función para manejar el scroll suave y respuesta rápida
         let isScrolling = false;
