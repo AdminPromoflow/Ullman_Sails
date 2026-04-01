1c55130a596d106cd8f315df133a3e959fee4dc3
Laura  Rozo <admin@promoflow.net>
2026-04-01 02:38:24 -0500

    “…”

--- cruising_downwind/index.php
+++ cruising_downwind/index.php
@@ -22,17 +22,17 @@
       width: 100vw;
       overflow-x: hidden;
     }
-  
+
     </style>
   </head>
   <body>
-    <?php include "../General/Menu/Menu.php"?>
+    <?php include "../general/menu/menu.php"?>
 
-    <?php include "../General/Charging/charging.php"; ?>
-    <?php include "../General/new_sail_quote/new_sail_quote.php"; ?>
+    <?php include "../general/charging/charging.php"; ?>
+    <?php include "../general/new_sail_quote/new_sail_quote.php"; ?>
 
 
-    <?php include "../General/arrows_up_down/arrows_up_down.php" ?>
+    <?php include "../general/arrows_up_down/arrows_up_down.php" ?>
 
     <?php include "../cruising_downwind/0_slider/slider.php";?>
     <?php include "navigation/navigation.php"?>
@@ -55,6 +55,6 @@
 
 
 
-    <?php include "../General/Footer/Footer.php"?>
+    <?php include "../general/footer/Footer.php"?>
   </body>
 </html>
