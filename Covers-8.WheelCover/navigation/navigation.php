a3c8f69f9140575d4e719b071e2a057d4fde64d4
Laura  Rozo <admin@promoflow.net>
2026-04-01 02:31:04 -0500

    “…”

--- Covers-8.WheelCover/navigation/navigation.php
+++ Covers-8.WheelCover/navigation/navigation.php
@@ -3,15 +3,12 @@ declare(strict_types=1);
 
 /* Filesystem paths (for filemtime) */
 $navCssFs = __DIR__ . '/Covers/navigation/navigation.css';
-$navJsFs  = __DIR__ . '/Covers/navigation/navigation.js';
 
 /* Public paths (as used in HTML) */
 $navCssPublic = '../Covers/navigation/navigation.css';
-$navJsPublic  = '../Covers/navigation/navigation.js';
 
 /* Version values (cache-busting) */
 $navCssV = is_file($navCssFs) ? filemtime($navCssFs) : time();
-$navJsV  = is_file($navJsFs)  ? filemtime($navJsFs)  : time();
 ?>
 
 <link rel="stylesheet" href="<?= $navCssPublic ?>?v=<?= $navCssV ?>">
@@ -52,5 +49,3 @@ $breadcrumbs = $breadcrumbs ?? [
     </ol>
   </nav>
 </section>
-
-<script defer src="<?= $navJsPublic ?>?v=<?= $navJsV ?>" type="text/javascript"></script>
