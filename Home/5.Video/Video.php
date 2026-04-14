e05e1df24e045a4a0a9e92b21a71af1592f25ef6
Aleina Rossui <admin@promoflow.net>
2026-04-14 04:49:47 -0400

    mensaje

--- Home/5.Video/Video.php
+++ Home/5.Video/Video.php
@@ -45,11 +45,9 @@ $jsTime = filemtime('../Home/5.Video/video.js');
       <div class="contactus-hero__actions">
         <a href="../ContactUs/index.php" class="contactus-btn contactus-btn--primary">Contact us</a>
         <a href="../New_Sail_Quote/index.php" class="contactus-btn contactus-btn--secondary">New Sail Quote</a>
-      </div>
-      <div class="contactus-hero__actions">
-        <a href="../ContactUs/index.php" class="contactus-btn contactus-btn--primary">New Repair Quote</a>
         <a href="../New_Repair_Quote/index.php" class="contactus-btn contactus-btn--secondary">New Repair Quote</a>
       </div>
+
     </div>
   </div>
 </section>
